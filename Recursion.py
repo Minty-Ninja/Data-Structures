fe0df7e8a1d28c0616ee51899043b5c80b26494b
Minty-Ninja <idhant.kumar10@gmail.com>
2025-03-15 13:06:02 +0530

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -8,3 +8,35 @@ def Fib(num):
     
 print (Fib(5))
      
+
+
+#Fibonacci series
+
+def Fib(num):
+    if num == 0 or num == 1: 
+        return num
+    else: 
+        return Fib(num-1) + Fib(num-2)
+    
+print (Fib(5))
+     
+
+#Factorials
+
+def fact(num): 
+    if num == 0 or num == 1: 
+        return 1
+    else: 
+        return num * fact(num-1)
+print (fact(4))
+
+#Sum to n
+def sum(num):
+    if num == 1 or num==0: 
+        return num
+    else: 
+        return num+sum(num-1)
+    
+print(sum(10))
+    
+
